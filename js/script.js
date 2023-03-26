8e9b7a5ce115720883836524a002350ecc353eb4
Tyom8 <119136182+Tyom8@users.noreply.github.com>
2023-03-26 08:51:02 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,218 @@
+
+let colors = ["red","green","blue","yellow","pink","purple"];
+let inter = setInterval(() => {
+   let randomColor = colors[Math.floor(Math.random() * colors.length)];
+   document.body.style.background = randomColor;
+   document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[8].childNodes[9].style.background = randomColor;
+   document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[14].childNodes[5].style.background = randomColor;
+   document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[28].childNodes[3].style.background = randomColor;
+},200)
+
+let i = 0;
+let inter2 = setInterval(() => {
+   let color2 = ["black","Red"];
+   if(i >= color2.length){
+         i = 0
+      }    let randomColor2 = color2[i];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[2].childNodes[7].style.background = randomColor2;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[1].style.background = randomColor2;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[10].childNodes[1].style.background = randomColor2;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[14].childNodes[7].style.background = randomColor2;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[16].childNodes[5].style.background = randomColor2;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[34].childNodes[5].style.background = randomColor2;
+      i++
+},200)
+
+let x = 0;
+let inter3 = setInterval(() => {
+   let color3 = ["yellow","purple","aquamarine"];
+   if(x >= color3.length){
+         x = 0
+      }    let randomColor3 = color3[x];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[3].style.background = randomColor3;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[4].childNodes[9].style.background = randomColor3;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[8].childNodes[7].style.background = randomColor3;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[10].childNodes[3].style.background = randomColor3;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[22].childNodes[3].style.background = randomColor3;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[32].childNodes[1].style.background = randomColor3;
+      x++
+},500)
+
+let y = 0;
+let inter4 = setInterval(() => {
+   let color4 = ["yellow","purple","aquamarine","red"];
+   if(y >= color4.length){
+         y = 0
+      }    let randomColor4 = color4[y];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[5].style.background = randomColor4;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[8].childNodes[5].style.background = randomColor4;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[6].childNodes[7].style.background = randomColor4;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[16].childNodes[1].style.background = randomColor4;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[20].childNodes[13].style.background = randomColor4;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[22].childNodes[1].style.background = randomColor4;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[24].childNodes[7].style.background = randomColor4;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[34].childNodes[3].style.background = randomColor4;
+      y++
+},900)
+
+let yx = 0;
+let inter5 = setInterval(() => {
+   let color5 = ["green","black"];
+   if(yx >= color5.length){
+         yx = 0
+      }    let randomColor5 = color5[yx];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[7].style.background = randomColor5;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[6].childNodes[5].style.background = randomColor5;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[12].childNodes[1].style.background = randomColor5;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[20].childNodes[11].style.background = randomColor5;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[24].childNodes[5].style.background = randomColor5;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[32].childNodes[3].style.background = randomColor5;
+      yx++
+},300)
+
+let yxy = 0;
+let inter6 = setInterval(() => {
+   let color6 = ["green","pink"];
+   if(yxy >= color6.length){
+         yxy = 0
+      }    let randomColor6 = color6[yxy];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[9].style.background = randomColor6;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[6].childNodes[1].style.background = randomColor6;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[8].childNodes[3].style.background = randomColor6;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[10].childNodes[5].style.background = randomColor6;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[16].childNodes[7].style.background = randomColor6;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[28].childNodes[1].style.background = randomColor6;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[32].childNodes[5].style.background = randomColor6;
+      yxy++
+},30)
+
+let c = 0;
+let inter7 = setInterval(() => {
+   let color7 = ["orange","yellow"];
+   if(c >= color7.length){
+         c = 0
+      }    let randomColor7 = color7[c];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[11].style.background = randomColor7;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[6].childNodes[3].style.background = randomColor7;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[10].childNodes[7].style.background = randomColor7;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[20].childNodes[9].style.background = randomColor7;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[26].childNodes[3].style.background = randomColor7;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[32].childNodes[7].style.background = randomColor7;
+      c++
+},200)
+
+let cp = 0;
+let inter8 = setInterval(() => {
+   let color8 = ["orange","yellow","green","black","blue","yellow","pink","purple"];
+   if(cp >= color8.length){
+         cp = 0
+      }    let randomColor8 = color8[cp];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.firstChild.childNodes[13].style.background = randomColor8;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[4].childNodes[7].style.background = randomColor8;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[8].childNodes[1].style.background = randomColor8;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[16].childNodes[9].style.background = randomColor8;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[20].childNodes[7].style.background = randomColor8;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[26].childNodes[5].style.background = randomColor8;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[30].childNodes[3].style.background = randomColor8;
+      cp++
+},600)
+
+
+
+let cpu = 0;
+let inter9 = setInterval(() => {
+   let color9 = ["blue","green"];
+   if(cpu >= color9.length){
+         cpu = 0
+      }    let randomColor9 = color9[cpu];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[2].childNodes[1].style.background = randomColor9;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[2].childNodes[9].style.background = randomColor9;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[14].childNodes[3].style.background = randomColor9;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[22].childNodes[5].style.background = randomColor9;
+      cpu++
+},400)
+
+let w = 0;
+let inter10 = setInterval(() => {
+   let color10 = ["gray","brown"];
+   if(w >= color10.length){
+            w = 0
+      }    let randomColor10 = color10[w];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[2].childNodes[3].style.background = randomColor10;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[4].childNodes[5].style.background = randomColor10;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[16].childNodes[11].style.background = randomColor10;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[18].childNodes[7].style.background = randomColor10;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[24].childNodes[3].style.background = randomColor10;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[30].childNodes[1].style.background = randomColor10;
+      w++
+},100)
+
+let wt = 0;
+let inter11 = setInterval(() => {
+   let color11 = ["gold","silver","plum","lavender","maroon","crimson","salmon","khaki"];
+   if(wt >= color11.length){
+            wt = 0
+      }    let randomColor11 = color11[wt];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[2].childNodes[5].style.background = randomColor11;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[4].childNodes[3].style.background = randomColor11;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[18].childNodes[5].style.background = randomColor11;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[20].childNodes[1].style.background = randomColor11;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[32].childNodes[9].style.background = randomColor11;
+      wt++
+},400)
+
+let wtr = 0;
+let inter12 = setInterval(() => {
+   let color12 = ["gold","silver","plum","lavender","maroon","crimson","salmon","khaki"];
+   if(wtr >= color12.length){
+            wtr = 0
+      }    let randomColor12 = color12[wtr];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[4].childNodes[1].style.background = randomColor12;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[6].childNodes[9].style.background = randomColor12;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[12].childNodes[3].style.background = randomColor12;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[18].childNodes[3].style.background = randomColor12;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[24].childNodes[1].style.background = randomColor12;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[26].childNodes[7].style.background = randomColor12;
+      wtr++
+},500);
+
+let a = 0;
+let inter13 = setInterval(() => {
+   let color13 = ["orange","yellow","green","black","blue","yellow","pink","purple","gold","silver","plum","lavender","maroon","crimson","salmon","khaki"];
+   if(a >= color13.length){
+            a = 0
+      }    let randomColor13 = color13[a];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[14].childNodes[1].style.background = randomColor13;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[20].childNodes[3].style.background = randomColor13;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[22].childNodes[7].style.background = randomColor13;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[34].childNodes[7].style.background = randomColor13;
+      a++
+},300);
+
+
+let as = 0;
+let inter13a = setInterval(() => {
+   let color13a = ["orange","yellow","green","black","blue","yellow","pink","purple","gold","silver","plum","lavender","maroon","crimson","salmon","khaki"];
+   if(as >= color13a.length){
+            as = 0
+      }    let randomColor13a = color13a[as];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[16].childNodes[3].style.background = randomColor13a;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[22].childNodes[9].style.background = randomColor13a;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[26].childNodes[1].style.background = randomColor13a;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[28].childNodes[5].style.background = randomColor13a;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[34].childNodes[1].style.background = randomColor13a;
+
+      as++
+},20);
+
+
+let asw = 0;
+let inter13as = setInterval(() => {
+   let color13as = ["navy blue","sky blue","lime green","teal","indigo","magenta"];
+   if(asw >= color13as.length){
+            asw = 0
+      }    let randomColor13as = color13as[asw];
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[18].childNodes[1].style.background = randomColor13as;
+      document.body.firstChild.nextSibling.firstChild.nextSibling.childNodes[20].childNodes[5].style.background = randomColor13as;
+      asw++
+},300);
